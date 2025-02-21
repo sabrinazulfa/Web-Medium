1a269a858e2a9cd189154410c4c2d47d235aa726
sabrinazulfa <sabrinazulfawa@gmail.com>
2025-02-21 13:01:36 +0700

    try to add footer 4 - main to

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,7 +96,7 @@
                 <div class="col-md-3">
                     <h5>Contact Us</h5>
                     <ul class="list-unstyled">
-                        <li>Email: <a href="mailto:info@edufair-event.com">info@edufair-event.com</a></li>
+                        <li>Email: <a href="mailto:info@edufair-event.com">admin@tomogaku.ip-ddns.com</a></li>
                         <li>Phone: +6285735160156</li>
                         <li>Address: Jakarta, Indonesia</li>
                     </ul>
