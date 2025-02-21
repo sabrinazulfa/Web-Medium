06346d2409ded26469d9fdd338b42430ddb4a903
sabrinazulfa <sabrinazulfawa@gmail.com>
2025-02-21 12:22:13 +0700

    add logo

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,6 +7,7 @@
     <title>TomoGaku</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/img/logo.webp') }}" type="image/x-icon">
 </head>
 
 <body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('assets/img/logo.webp') }}" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Registration Form</title>
+    <title>Registration Form | Tomogaku</title>
     <style>
         .form-container {
             display: flex;
